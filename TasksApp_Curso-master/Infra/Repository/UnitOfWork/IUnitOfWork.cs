049260577d67ec8ec601a0981251aae73aefcd8f
leandroscudeler <leandroscudeler@gmail.com>
2025-07-15 16:35:36 -0300

    Curso APIs RESTful robustas

---
+++ TasksApp_Curso-master/Infra/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Infra.Repository.IRepositories;
+
+namespace Infra.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        void Commit();
+    }
+}
---
+++ TasksApp_Curso-master/Infra/Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Infra.Persistence;
+using Infra.Repository.IRepositories;
+using Infra.Repository.Repositories;
+
+namespace Infra.Repository.UnitOfWork
+{
+    public class UnitOfWork(TasksDbContext context, IUserRepository userRepository) : IUnitOfWork
+    {
+        private readonly TasksDbContext _context = context;
+
+        public IUserRepository UserRepository => userRepository ?? new UserRepository(context);
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
